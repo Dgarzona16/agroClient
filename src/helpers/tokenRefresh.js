2c94b367250d7f484e14d53088356ea4ec3792ac
Dgarzona16 <00271419@uca.edu.sv>
2022-11-30 22:37:31 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/helpers/tokenRefresh.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+
+export const tokenRefresh = async () => {
+    const uri = import.meta.env.VITE_AGRO_API;
+    const config = {
+        url: `${uri}auth/refresh`,	
+        method: 'GET',
+        Credential: 'include',
+    }
+    try {
+        const response = await axios(config);
+        return response.data.token;
+    }
+    catch (error) {
+        return false;
+    }
+};
